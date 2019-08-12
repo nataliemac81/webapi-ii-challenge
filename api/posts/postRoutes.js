5d9b7570a859346249ddebb4f4b40f8e9973f33e
Natalie McCroy <nataliemccroy@gmail.com>
2019-08-11 21:14:22 -0500

    endpoint for GET /api/posts/:id  working

--- api/posts/postRoutes.js
+++ api/posts/postRoutes.js
@@ -11,13 +11,31 @@ router.get('/', (req, res) => {
   })
   .catch(err => {
 	res.status(500).json({
-		err: err
+		err: err,
+		message: "The posts information could not be retrieved."
 	})
   })
 })
 
 router.get('/:id', (req, res) => {
-	res.status(200).send('hello from the GET /posts/:id endpoint')
+	const { id } = req.params
+	
+	db.findById(id)
+	.then(post => {
+		if(post) {
+			res.status(200).json(post)
+		} else {
+			res.status(404).json({
+				message: "The post with the specified ID does not exist."
+			})
+		}
+	})
+	.catch(err => {
+		res.status(500).json({
+			err: err,
+			message: "The post information could not be retrieved."
+		})
+	})
 })
 
 router.get('/:id/comments', (req, res) => {
